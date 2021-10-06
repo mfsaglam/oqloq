bf73e7449ad04ac1db5923f0effe132b87004357
mfsaglam <mfsaglam1@gmail.com>
2021-10-06 17:03:33 +0300

    Initial Commit

---
+++ Oqloq/OqloqApp.swift
@@ -0,0 +1,17 @@
+//
+//  OqloqApp.swift
+//  Oqloq
+//
+//  Created by Fatih Sağlam on 6.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct OqloqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
